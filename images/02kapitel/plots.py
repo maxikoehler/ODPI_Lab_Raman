71c318a0f6b6a1e3f41ef8ca19a3c8f8ccf1cf34
maxikoehler <64246754+maxikoehler@users.noreply.github.com>
2024-02-05 10:33:01 +0100

    2024-02-05 10:33 Status Update

--- images/02kapitel/plots.py
+++ images/02kapitel/plots.py
@@ -2,8 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
-
+import scipy as sp
 
 # redefining plot save parameters
 plt.rcParams.update({
@@ -13,13 +12,7 @@ plt.rcParams.update({
     "font.size": 12
 })
 
-matplotlib.use("pgf")
-matplotlib.rcParams.update({
-    "pgf.texsystem": "pdflatex",
-    'font.family': 'serif',
-    'text.usetex': True,
-    'pgf.rcfonts': False,
-})
+# matplotlib.use("pgf")
 
 # defining the isosbestic point
 isosbestic_point = 3390
@@ -28,37 +21,44 @@ isosbestic_point = 3390
 temp_data_raw = pd.read_csv('water-temperature_data-raw.csv', sep=';')
 temp_data_norm = pd.read_csv('water-temperature_data-norm.csv', sep=';')
 temp_time = pd.read_csv('water-temperature_data-time.csv', sep=';')
+data_spectrum = pd.read_csv('plot-data-spectrum.csv', sep=';')
+data_molar = pd.read_csv('plot-data-molar.csv', sep=';')
+data_intersec = pd.read_csv('plot-intersec.csv', sep=';')
 
 temp_data_norm.set_index('Wellenlange', inplace=True)
 temp_data_raw.set_index('Wellenlange', inplace=True)
 temp_time.set_index('time', inplace=True)
+data_spectrum.set_index('raman_shift', inplace=True)
+data_molar.set_index('xETOH', inplace=True)
+data_intersec.set_index('raman_shift', inplace=True)
 
 temp_data_raw[1:] = temp_data_raw[1:]/1000000
+data_spectrum[1:] = data_spectrum[1:]/1000000
+data_intersec[1:] = data_intersec[1:]/10000
 
-
-# Plot of raw data
-# temp_data_raw['0'].plot(label='lowest temperature')
-# temp_data_raw['70'].plot(label='highest temperature')
+# # Plot of raw data
+# temp_data_raw['0'].plot(label='$T_\mathrm{K}=293,35~\mathrm{K}$')
+# temp_data_raw['70'].plot(label='$T_\mathrm{K}=346,65~\mathrm{K}$')
 # plt.axvline(x = isosbestic_point, color='purple', linestyle='--', label='isosbestic point')
-# plt.xlabel('Raman shift $\Delta \tilde{v}$ in $\mathrm{cm^{-1}$')
-# plt.ylabel('Signal intensity')
+# plt.xlabel('Raman shift $\Delta v$ in $\mathrm{cm}^\mathrm{-1}$')
+# plt.ylabel('Signal intensity in $10^6$')
 # plt.grid()
 # plt.legend()
 # plt.savefig('temp_data_raw.pgf')
+# plt.close()
 
-
-
-# Plot of normalized data
-# temp_data_norm['0'].plot(label='lowest temperature')
-# temp_data_norm['70'].plot(label='highest temperature')
+# # Plot of normalized data
+# temp_data_norm['0'].plot(label='$T_\mathrm{K}=293,35~\mathrm{K}')
+# temp_data_norm['70'].plot(label='$T_\mathrm{K}=346,65~\mathrm{K}')
 # plt.axvline(x = isosbestic_point, color='purple', linestyle='--', label='isosbestic point')
-# plt.xlabel('Raman shift')# ' in $cm$')
+# plt.xlabel('Raman shift $\Delta v$ in $\mathrm{cm}^\mathrm{-1}$')
 # plt.ylabel('Normalized signal intensity')
 # plt.grid()
 # plt.legend()
 # plt.savefig('temp_data_norm.pgf')
+# plt.close()
 
-# Plot of temperatures from thermocouple and raman
+# # Plot of temperatures from thermocouple and raman
 # temp_time['T_thermocouple'].plot(label='T by thermocouple')
 # temp_time['T_raman'].plot(label='T by Raman')
 # plt.xlabel('Time in $\mathrm{s}$')
@@ -66,3 +66,48 @@ temp_data_raw[1:] = temp_data_raw[1:]/1000000
 # plt.grid()
 # plt.legend()
 # plt.savefig('temp_time.pgf')
+# plt.close()
+
+# # Plot of spectrum e00, e10 and unknown
+# data_spectrum['e00'].plot(label='spectrum $x=0,0$')
+# data_spectrum['e10'].plot(label='spectrum $x=1,0$')
+# data_spectrum['e_unknown'].plot(label='spectrum $x=\mathrm{unknown}$')
+# plt.legend()
+# plt.grid()
+# plt.xlabel('Raman shift $\Delta v$ in $\mathrm{cm}^\mathrm{-1}$')
+# plt.ylabel('Siganl intensity in $10^6$')
+# plt.savefig('spectra.pgf')
+# plt.close()
+
+# Plot of the determination integration limits
+intersec = data_intersec['stw'].to_list()
+plt.plot(data_intersec.index, intersec, '*', color='orange', label='standard deviation of all spectra')
+plt.legend()
+plt.xlabel('Raman shift $\Delta v$ in $\mathrm{cm}^\mathrm{-1}$')
+plt.ylabel('standard deviation in $10^4$')
+plt.ylim(bottom=0)
+plt.grid()
+plt.savefig('stw.pgf')
+plt.close()
+
+# # Plot of calibration curve
+# calib_data = data_molar.drop('unknown', axis=0)
+# calib_lst = calib_data['signal_ratio'].tolist()
+# calib_x = [0,0.1,0.2,0.4,0.6,0.8,1]
+
+# def fit_func(x, m):
+#     y = m*x
+#     return y
+
+# m, z = sp.optimize.curve_fit(fit_func, calib_x, calib_lst)
+
+# plt.plot(calib_x, calib_lst, '.', label='signal ratio calibration')
+# plt.plot(calib_x, fit_func(calib_x, m), '--', label='fit function')
+# plt.text(0.7, 0.25, 'linear fit:\ny(x)= '+str(np.round(m[0], 3))+' $\cdot$ x')
+# plt.legend()
+# plt.grid()
+# plt.xlabel('Molar ethanol content')
+# plt.ylabel('Signal ratio')
+# plt.ylim(bottom=0, top=1.1)
+# plt.savefig('calibration.pgf')
+# plt.close()
